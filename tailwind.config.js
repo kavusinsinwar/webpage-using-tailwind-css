23f3e8cd40842cae62b0da8617ba9154206c2bca
kavusinsinwar <kavusinsinwar@gmail.com>
2024-11-19 15:22:58 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      fontFamily:{
+        'display':['poppins','sans-serif'],
+        body:[ 'Inter','sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
